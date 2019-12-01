1b4d172e80567d749ec645c370b694108547eabf
Marek Sterzik <marek@milimetr.org>
2019-12-01 12:27:50 +0100

    added the Geometry object containing multiple methods solving geometrical problems

--- js/model.js
+++ js/model.js
@@ -147,7 +147,7 @@ ValveGearModel.prototype._setupCalibration = function()
     bottomRangeVector = bottomRangeVector.normalize().mul(expansionLinkRange);
     this.calibration.expansionLinkBottomEnd = this.calibration.expansionLinkFixed.addVector(bottomRangeVector);
 
-    this.calibration.expansionLinkRadiusCenter = this.circleCenterFrom3Points(
+    this.calibration.expansionLinkRadiusCenter = Geometry.circleCenterFrom3Points(
         this.calibration.expansionLinkTopEnd,
         this.calibration.expansionLinkFixed,
         this.calibration.expansionLinkBottomEnd
@@ -341,51 +341,3 @@ ValveGearModel.prototype._updateAverage = function (id, value)
     return this.averages[id];
 }
 
-ValveGearModel.prototype.circleCenterFrom3Points = function(a, b, c)
-{
-    var va = b.vectorTo(a);
-    var vb = b.vectorTo(c);
-    var A = b.addVector(va.mul(.5));
-    var B = b.addVector(vb.mul(.5));
-
-    var matrix = [
-        [va.x, va.y, va.x * A.x + va.y * A.y],
-        [vb.x, vb.y, vb.x * B.x + vb.y * B.y]
-    ];
-
-    var solution = this.solve2Matrix(matrix);
-
-    return new Point(solution[0], solution[1]);
-}
-
-
-ValveGearModel.prototype.solve2Matrix = function(matrix)
-{
-    if (Math.abs(matrix[0][0]) < Math.abs(matrix[1][0])) {
-        var x = matrix[0];
-        matrix[0] = this.matrixLinearCombination(matrix, [1/matrix[0][0], 0]);
-        matrix[1] = this.matrixLinearCombination(matrix, [-matrix[1][0], 1]);
-        matrix[1] = this.matrixLinearCombination(matrix, [0, 1/matrix[1][1]]);
-        matrix[0] = this.matrixLinearCombination(matrix, [1, -matrix[0][1]]);
-    }
-
-    return [matrix[0][2], matrix[1][2]];
-}
-
-ValveGearModel.prototype.matrixLinearCombination = function (matrix, lineCoefficients)
-{
-    var rows = matrix.length;
-    var columns = matrix[0].length;
-    var finalVector = [];
-    for (var j = 0; j < columns; j++) {
-        finalVector[j] = 0;
-    }
-    
-    for (var i = 0; i < rows; i++) {
-        for (var j = 0; j < columns; j++) {
-            finalVector[j] += lineCoefficients[i] * matrix[i][j];
-        }
-    }
-
-    return finalVector;
-}

24aa6ab6d4430da9bc1647ef269df787051dedf5
Marek Sterzik <marek.sterzik@gmail.com>
2025-04-27 18:45:02 +0200

    stats subsystem simplified

--- src/model/walschaerts.js
+++ src/model/walschaerts.js
@@ -16,7 +16,6 @@ export default class
         this.data.param("expansion", 1)
 
         //statistics and averages
-        this.statistics = []
         this.averages = {}
         this.averageCycles = 10
 
@@ -48,54 +47,20 @@ export default class
 
     recalc()
     {
-        var t0 = performance.now()
-        
-        var statistics = []
-
-        //push the total solve time slot into the statistics
-        //the real value will be computed last
-        statistics.push({
-            "model": "total",
-            "param": "solveTime",
-            "value": 0,
-        })
-
-        var lt0 = performance.now()
         this.data.apply(walschaertsModel)
-        var lt1 = performance.now()
         
-
-        statistics.push({
-            "model": "model1",
-            "param": "solveTime",
-            "value": lt1 - lt0
-        })
         const modelStats = this.data.allStats()
-        for (var s in modelStats) {
-            statistics.push({
-                "model": "model",
-                "param": s,
-                "value": modelStats[s],
-            })
+        for (var key in modelStats) {
+            this.updateAverage(key, modelStats[key])
         }
-
-        for (var i = 1; i < statistics.length; i++) {
-            statistics[i].value = this._updateAverage(statistics[i].model+"."+statistics[i].param, statistics[i].value)
-        }
-        statistics[0].value = performance.now() - t0
-        statistics[0].value = this._updateAverage(statistics[0].model+"."+statistics[0].param, statistics[0].value)
-
-        this.statistics = statistics
-
     }
 
-    _updateAverage (id, value)
+    updateAverage (id, value)
     {
         if (! (id in this.averages)) {
             this.averages[id] = value
         } else {
             this.averages[id] = this.averages[id] + (value - this.averages[id])/this.averageCycles
         }
-        return this.averages[id]
     }
 }
--- src/view/stats.js
+++ src/view/stats.js
@@ -16,49 +16,15 @@ export default class
             var statString = ''
             statString += "<h2>mechanic model statistic averages</h2>"
             statString += "<ul>"
-            for (var i = 0; i < this.model.statistics.length; i++) {
-                var statRecord = this.extendRecord(this.model.statistics[i])
+            Object.keys(this.model.averages).sort().forEach((key) => {
                 statString += "<li>"
-                statString += statRecord.paramFull+"="
-                statString += "<strong>"+statRecord.valueFull+"</strong>"
+                statString += key+"="
+                statString += "<strong>"+this.round(this.model.averages[key], 1)+"</strong>"
                 statString += "</li>"
-            }
+            })
             statString += "</ul>"
             this.div.html(statString)
             this.lastUpdateTime = now
-        }
-    }
-
-
-    extendRecord(statRecord)
-    {
-        var finalStatRecord = {}
-        finalStatRecord.model = statRecord.model
-        finalStatRecord.param = statRecord.param
-        finalStatRecord.paramFull = statRecord.model+"."+statRecord.param
-        finalStatRecord.value = statRecord.value
-        finalStatRecord.rounding = this.getRecordRounding(statRecord)
-        finalStatRecord.unit = this.getRecordUnit(statRecord)
-        finalStatRecord.valueFull = this.round(finalStatRecord.value, finalStatRecord.rounding) + "" + finalStatRecord.unit
-
-        return finalStatRecord
-    }
-
-    getRecordRounding(statRecord)
-    {
-        if (statRecord.param == 'error') {
-            return 4
-        } else {
-            return 1
-        }
-    }
-
-    getRecordUnit(statRecord)
-    {
-        if (statRecord.param == 'solveTime' || statRecord.param == 'totalTime') {
-            return 'ms'
-        } else {
-            return ''
         }
     }
 
